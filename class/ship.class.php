cf09806e6d8a9e06623dcf23f50644488425fd2e
PazBorma <57680906+PazBorma@users.noreply.github.com>
2025-06-01 18:43:21 +0200

    readd;

--- class/ship.class.php
+++ class/ship.class.php
@@ -1756,7 +1756,9 @@ class ship
 		return $return;
 	}
 	
-	function getclasses() { return $this->db->query("SELECT * FROM stu_ships_rumps WHERE view=1 ORDER BY sorta ASC,sortb ASC",2); }
+	function getClasses() { 
+		return $this->db->query("SELECT * FROM stu_ships_rumps WHERE view=1 ORDER BY sorta ASC,sortb ASC",2); 
+	}
 	
 	function getnpcclasses() { return $this->db->query("SELECT * FROM stu_ships_rumps ORDER BY sorta ASC,sortb ASC",2); }
 	
