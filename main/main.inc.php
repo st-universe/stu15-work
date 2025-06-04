885cebcc8227c768f074f0b4950c76e44374ccc8
Huxinator <nicotintel@gmail.com>
2025-06-04 14:00:32 +0200

    update

--- main/main.inc.php
+++ main/main.inc.php
@@ -1,18 +1,30 @@
 <table width=100% bgcolor=#262323 cellspacing=1 cellpadding=1>
-<tr>
-	<td class=tdmain>/ <a href=page=main>STU</a> / <strong>Willkommen...</strong></td>
-</tr>
+	<tr>
+		<td class=tdmain>/ <a href=page=main>STU</a> / <strong>Willkommen...</strong></td>
+	</tr>
 </Table><br>
 <table width=100% bgcolor=#262323 cellspacing=1 cellpadding=1>
-<tr>
-	<td class=tdmainobg>... in den Weiten von Star Trek Universe.<br>
-	Star Trek Universe ist ein kostenloses, rundenbasierendes Onlinestrategiespiel. Eine genauere Beschreibung findest Du <a href=http://wiki.stuniverse.de/index.php/STU target=_blank><u>hier</u></a>.<br><br>
-	Bist Du neu hier?<br>
-	- <a href=http://wiki.stuniverse.de target=_blank><u>Hilfe-Wiki</u></a><br>
-	- <a href=?page=rules><u>Regeln</u></a> (!)<br>
-	- <a href=?page=register><u>Registrierung</u></a><br><br>
-	<b>05.05.06</b> - Das war es nun. Die aktuellen News zum STU2 Start bekommt ihr im Forum.<br>Bis bald in STU2!<br><br>
-	<b>12.03.06</b> - <img src=http://www.stuniverse.de/chaos.jpg><br><font color=red>Morgen beginnt das Chaos.</font> Details im Forum: <a href=http://forum.stuniverse.de/viewtopic.php?p=22368  target=_blank>http://forum.stuniverse.de/viewtopic.php?p=22368</a><br><br>	
-	<b>09.03.06</b> - An der Warenbörse gibt es jetzt die Option Merchandising-Artikel zu kaufen. Dabei handelt es sich um "Actionfiguren" als Waren, die 25 Latinum pro Stück kosten. Wer also nichts mit seinem Latinum anzufangen weiß, kann ja versuchen, alle Figuren zu sammeln ^^<br><br>
-</tr>
+	<tr>
+		<td class=tdmainobg>... in den Weiten von Star Trek Universe.<br>
+			Star Trek Universe ist ein kostenloses, rundenbasierendes Onlinestrategiespiel. Eine genauere Beschreibung
+			findest Du <a href=http://wiki.stuniverse.de/index.php/STU target=_blank><u>hier</u></a>.<br><br>
+			Bist Du neu hier?<br>
+			- <a href=http://wiki.stuniverse.de target=_blank><u>Hilfe-Wiki</u></a><br>
+			- <a href=?page=rules><u>Regeln</u></a> (!)<br>
+			- <a href=?page=register><u>Registrierung</u></a><br><br>
+			<b>01.06.25</b> - Das war es noch nicht! Dies ist die Archiv Version von STU v1.5<br>Hierbei handelt es sich
+			um das Spiel mit seinem Stand vom 05.05.2006. Dies betrifft auch die Datenbank.<br>Alte Spieleraccounts
+			existieren noch, jedoch sind die ZugÃ¤nge aus SicherheitsgrÃ¼nden gelÃ¶scht.
+			<br>Wer seinen alten Account wieder erkennt kann sich bei Hux <a href=https://discord.gg/NK3fCdx
+				target="https://discord.gg/NK3fCdx">auf dem offiziellen Discord Server von STU</a> <br><br>
+			<b>05.05.06</b> - Das war es nun. Die aktuellen News zum STU2 Start bekommt ihr im Forum.<br>Bis bald in
+			STU2!<br><br>
+			<b>12.03.06</b> - <img src=http://www.stuniverse.de/chaos.jpg><br>
+			<font color=red>Morgen beginnt das Chaos.</font> Details im Forum: <a
+				href=http://forum.stuniverse.de/viewtopic.php?p=22368
+				target=_blank>http://forum.stuniverse.de/viewtopic.php?p=22368</a><br><br>
+			<b>09.03.06</b> - An der Warenbï¿½rse gibt es jetzt die Option Merchandising-Artikel zu kaufen. Dabei handelt
+			es sich um "Actionfiguren" als Waren, die 25 Latinum pro Stï¿½ck kosten. Wer also nichts mit seinem Latinum
+			anzufangen weiï¿½, kann ja versuchen, alle Figuren zu sammeln ^^<br><br>
+	</tr>
 </table>
